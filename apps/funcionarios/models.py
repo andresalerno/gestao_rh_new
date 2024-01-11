ecff1a3c53d507c63b2aae7137414360a1461750
andresalerno <andresalerno22@gmail.com>
2024-01-11 18:27:57 -0300

    aula 16 crud empresa

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -5,9 +5,10 @@ from apps.empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
-    user = models.OneToOneField(User, on_delete=models.PROTECT) # se você quiser deletar, primeiro o funcionário e depois o usuário
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
-    
+    empresa = models.ForeignKey(
+        Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
     def __str__(self):
         return self.nome
